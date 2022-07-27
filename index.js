1ee50d3331118c369beed17496219ce78be528eb
rajneeshkshwh <mauryarajneesh96@gmail.com>
2022-07-27 20:04:33 +0530

    aa

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
-// const con = mysql.createConnection({
-//     host: 'alrpacomp.mysql.database.azure.com',
-//     user: 'comp_admin',
-//     password: 'W6AuaBRLZeehwQycLdRE',
-//     // database: 'judgeportal',
-//     port: "3306"
-// });
-// const con = mysql.createConnection({
-//     host: 'remotemysql.com',
-//     user: 'E939GyEwfl',
-//     password: 'P2gnSj7Fbg',
-//     database: 'E939GyEwfl',
-//     port: "3306"
-// });
+const con = mysql.createConnection({
+    host: 'remotemysql.com',
+    user: 'E939GyEwfl',
+    password: 'P2gnSj7Fbg',
+    database: 'E939GyEwfl',
+    port: "3306"
+});
 
-// con.connect(function(err) {
-//     if(err) throw err;
-//     console.log("connected!");
-// })
+setTimeout(function () { 
+    con.connect(function(err) {
+        if(err) throw err;
+        console.log("connected!");
+    })
+ }, 5000);
+
+
 
 app.use(function (req, res, next) {
     //Enabling CORS
@@ -56,21 +53,21 @@ app.post('/checkcode', (req,res) => {
     }
 })
 
-// app.get('/getindividualdata', (req,res) => {
-//     con.query("SELECT * FROM judges", function (err, result, fields) {
-//         if (err) throw err;
-//         res.send(result);
-//       });
-// })
+app.get('/getindividualdata', (req,res) => {
+    con.query("SELECT * FROM judges", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
+})
 
-// app.post('/getindividualdatabyid', (req,res) => {
-//     const id = req.body.id;
-//     var sql = 'SELECT * FROM judges WHERE id = ' + mysql.escape(id);
-//     con.query(sql, function (err, result, fields) {
-//         if (err) throw err;
-//         res.send(result);
-//       });
-// })
+app.post('/getindividualdatabyid', (req,res) => {
+    const id = req.body.id;
+    var sql = 'SELECT * FROM judges WHERE id = ' + mysql.escape(id);
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on ${port}`);
