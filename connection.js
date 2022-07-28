822dde298e31fed075080389882a107e5d07b761
rajneeshkshwh <mauryarajneesh96@gmail.com>
2022-07-28 09:52:00 +0530

    corrected connection

--- connection.js
+++ connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
 
-// var connection = mysql.createPool({
-//     host: 'remotemysql.com',
-//     user: 'E939GyEwfl',
-//     password: 'P2gnSj7Fbg',
-//     database: 'E939GyEwfl',
-//     port: "3306"
-// });
 var connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'alphalake'
+    host: 'remotemysql.com',
+    user: 'E939GyEwfl',
+    password: 'P2gnSj7Fbg',
+    database: 'E939GyEwfl',
+    port: "3306"
 });
+// var connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'alphalake'
+// });
 
 
 module.exports = connection;
